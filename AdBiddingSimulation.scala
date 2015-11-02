bfd6d7f0b515cf70731d7d7ad6eced5eb87b8ce7
Jian Qiao <qiaojianjack@gmail.com>
2015-11-01 17:39:48 -0800

    added template for AdBiddingSimulation

---
+++ AdBiddingSimulation.scala
@@ -0,0 +1,32 @@
+# imports here
+
+class AdBiddingSimulation {
+
+  def init(adModel: Model, userModel: Model, quality_func: {(float, float) => float}) {
+
+  }
+
+  def simulate(keyPhrases: List[String], bids: Map[String => Float]) {
+
+  }
+
+  def getQualityScore( ) {
+
+  }
+
+  def getRanking() {
+
+  }
+
+  def getCTR() {
+
+  }
+
+  def calculateProfit() {
+
+  }
+
+  def aggregateMetrics() {
+
+  }
+}
