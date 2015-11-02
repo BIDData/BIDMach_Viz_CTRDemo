a05990e48ddcefbade13a245d0ad15e961bee1e7
Ryan Casey <rcaseyatbat@gmail.com>
2015-11-01 18:11:11 -0800

    initial getCTR code

--- AdBiddingSimulation.scala
+++ AdBiddingSimulation.scala
@@ -18,8 +18,20 @@ class AdBiddingSimulation {
 
   }
 
-  def getCTR() {
+  def getCTR(advertiser: String, keyword: String, rank: Int): Float = {
+    
+    // TODO: Instead of random, read from ad*kw row vector
+    // (assuming we have the mapping from ad*kw -> index)
+    val ad_kw_CTR = scala.util.Random.nextFloat
 
+    // TODO: Instead of random, read from rank column vector
+    // (just use rank as the index)
+    val rank_CTR = scala.util.Random.nextFloat
+
+    // Estimated CTR of this (ad, keyword) at this rank
+    var CTR = ad_kw_CTR * rank_CTR
+
+    return CTR
   }
 
   def calculateProfit() {
@@ -30,3 +42,4 @@ class AdBiddingSimulation {
 
   }
 }
+
