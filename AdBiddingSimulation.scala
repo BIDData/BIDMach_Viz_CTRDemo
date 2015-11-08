30c3150d9d0a133ea95d2ef1cff6ea53a00d93d1
Jian Qiao <qiaojianjack@gmail.com>
2015-11-07 21:42:16 -0800

    added sketch version of simulate()

--- AdBiddingSimulation.scala
+++ AdBiddingSimulation.scala
@@ -1,12 +1,17 @@
-import scala.collection.mutable
-import scala.collections.mutable.Array
-import scala.collections.HashMap
+import scala.collection.mutable.HashMap
 
 
 class AdBiddingSimulation(adModel: CTRModel, userModel: CTRModel, quality_func: (Float, Float) => Float) {
 
-  def simulate(keyPhrases: Array[String], bids: mutable.HashMap[String => Float]) {
-
+  def simulate(keyPhrases: Array[String], bids: HashMap[String => Float]) {
+    val totalProfits = keyPhrases.map((keyPhrase: String) =>  {
+      val qualityScores = bids.map((advertiser: String) => {getQualityScore(advertiser, keyPhrase, bids(advertiser))})
+      val ranks = getRanking(qualityScores)
+      val finalQuality = getFinalQualityScore(keyPhrase, ranks, bids)
+      val profits = ranks.map((rank: Int) => (calculateProfit(finalQuality, rank)))
+      //TODO: what to do after getting profits for each key phrase?
+      profits.foldleft(0.0)(_+_)
+    }
   }
 
   def getQualityScore( ) {
