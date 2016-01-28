5638d6663be293a91597a357b13e7d1a5fe1defd
Jian Qiao (Jack) <jian.qiao@berkeley.edu>
2016-01-27 23:15:18 -0800

    rewrite simulation code to take input data of matrix format

--- src/simulation/AdBiddingSimulation.scala
+++ src/simulation/AdBiddingSimulation.scala
@@ -1,50 +1,110 @@
 package simulation
 
 import akka.actor.Actor
+import BIDMat.{Dict, IDict, FMat, IMat, SMat, SBMat}
+import BIDMat.MatFunctions._
+import BIDMat.SciFunctions._
 import scala.collection.immutable.Map
 import scala.collection.immutable.ListMap
 
-import simulation.CTRModel
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.native.Serialization._
+import org.json4s.native.Serialization
+
+import scala.collection.mutable
 
 class AdBiddingSimulation(adModel: CTRModel, userModel: CTRModel, alpha: Float, beta: Float, actor: Actor) {
 
-  //
-  def run() = {
+  /**
+    * start the simulation cycle.
+    *
+    * @param dataPath The path to read record data.
+    * @param advertiserMap a BIDMat Dict between advertiser string and its integer id.
+    * @param keyPhraseMap a BIDMat Dict between key phrase string and its integer id.
+    */
+  def run(dataPath: String, advertiserMap: Dict, keyPhraseMap: Dict) = {
+    //TODO: where/how to get data?
 
+    //TODO: how to check hasData()? Might need to be implemented on Server Side
+    while (hasData()) {
+      val records = getBatchData() //Data should come from disk, in the form of BIDMach matrix
+      //TODO: keyWordMap and keyPhraseMap should be BIDMat Dict
+      val metrics = simulate(records, keyWordMap, keyPhraseMap)
+      sendMetrics(metrics)
+    }
   }
 
 
   def updateParams(newAlpha: Float, newBeta: Float) = {
-
+    alpha = newAlpha
+    beta = newBeta
   }
 
 
-  def qualityFunc(CTR: Float, bid: Float) = {(math.pow(CTR, alpha) * math.pow(bid, beta)).toFloat}
+  def qualityFunc(CTR: Float, bid: Float): Float = {
+    (math.pow(CTR, alpha) * math.pow(bid, beta)).toFloat
+  }
 
-  def invQualityFunc(quality: Float, CTR: Float) = {math.pow(quality / math.pow(CTR, alpha), 1 / beta).toFloat}
+  def invQualityFunc(quality: Float, CTR: Float): Float  = {
+    math.pow(quality / math.pow(CTR, alpha), 1 / beta).toFloat
+  }
 
-  def simulate(key_bid: Map[String, Map[String, Float]]) : Map[String, Float] = {
-    val totalProfits = key_bid map {
-      case (keyPhrase: String, bids: Map[String, Float]) => {
-        val qualityScores = bids map {
-          case (advertiser: String, bid: Float) => {
-            (advertiser, getQualityScore(advertiser, keyPhrase, bid))
-          }
-        }
-        val ranks = getRanking(qualityScores)
-        val finalQuality = getFinalQualityScores(keyPhrase, ranks, bids)
-        val profits = ranks map {
-          case (advertiser: String, rank: Int) => {
-            (advertiser, calculateProfit(finalQuality, keyPhrase, advertiser, rank))
-          }
-        }
+  /**
+    * simulate ad auctions on a batch of records.
+    *
+    * @param records an FMat with bidding records, potentially from multiple auctions.
+    * @param advertiserMap a BIDMat Dict between advertiser strings and their integer id in data.
+    * @param keyPhraseMap a BIDMat Dict between key phrase strings and their integer id in data.
+    * @return a list of metric results from each auction
+    */
 
-        //TODO: what to do after getting profits for each key phrase?
-        (keyPhrase, aggregateProfits(profits))
+  def simulate(records: FMat, advertiserMap: Dict, keyPhraseMap: Dict) : String = {
+    val uniqueKeyPhraseHash = unique(records(kp_hash)) //TODO: find which column/row is kp_hash
+    val i = 0
+    val results = mutable.MutableList[Map]()
+    while (i < uniqueKeyPhraseHash.nrows) {
+      val keyPhraseHash = uniqueKeyPhraseHash(i, :)
+      val auction = records (:, records(kp_hash) == keyPhraseHash)
+      results += simulateAuction(auction, advertiserMap, keyPhraseMap))
+    }
+    results
+  }
+
+
+  /**
+    * simulate an auction.
+    *
+    * @param auction an FMat containing the bidding records.
+    * @param advertiserMap a BIDMat Dict between advertiser strings and their integer id in data.
+    * @param keyPhraseMap a BIDMat Dict between key phrase strings and their integer id in data.
+    * @return a Map containing the metric results.
+    */
+
+  def simulateAuction(auction: FMat, advertiserMap: Dict, keyPhraseMap: Dict) : Map = {
+    val keyPhrase = keyPhraseMap(auction(kp_col)) //TODO: find which col is for keyPhrase
+
+    val bids = Map(auction(:, advertiser_col).copyToFloatArray() zip auction(:, bid_col).copyToFloatArray())
+
+    val qualityScores = bids map {
+      case (advertiserHash: Float, bid: Float) => {
+        val advertiser = advertiserMap(advertiserHash)
+        (advertiser, getQualityScore(advertiser, keyPhrase, bid))
       }
     }
-    totalProfits.toMap
+    val ranks = getRanking(qualityScores)
+    val finalQuality = getFinalQualityScores(keyPhrase, ranks, bids)
+    val profits = ranks map {
+      case (advertiser: String, rank: Int) => {
+        (advertiser, calculateProfit(finalQuality, keyPhrase, advertiser, rank))
+      }
+    }
+
+    //TODO: could add more metrics, such as volume of ads, ads per advertiser, etc.
+    Map("keyPhrase" -> keyPhrase, "profit" -> aggregateProfits(profits))
   }
+
+
 
   def getQualityScore(advertiser: String, keyPhrase: String, bid: Float): Float = {
     val myCTR:Float = adModel.getCTR(1, advertiser, keyPhrase)
@@ -92,8 +152,10 @@ class AdBiddingSimulation(adModel: CTRModel, userModel: CTRModel, alpha: Float, 
     profits.values.sum
   }
 
-  def sendMetrics() = {
-
+  def sendMetrics(metrics: mutable.MutableList[Map]) = {
+    val jsonStr = compact(render(metrics))
+    actor ! jsonStr
   }
+
 
 }
